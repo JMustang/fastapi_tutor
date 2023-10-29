8d85da695d3ea7bd33ea28256fac45e4a2716c47
Junior <eecfredes@gmail.com>
2023-10-28 23:51:11 -0300

    testando se as libs estao funcionando

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
-from fastapi import FastAPI
+from fastapi import Body, FastAPI
 
 app = FastAPI()
+
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/post")
+async def get_posts():
+    return {"data": "This is your posts!"}
+
+
+@app.post("/post")
+async def create_posts(payload: dict = Body(...)):
+    print(payload)
+    return {"post": f"title {payload['title']}"}
