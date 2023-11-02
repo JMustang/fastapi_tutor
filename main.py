e869202cb5588596ac0f49e1048f6467cc8f12dd
Junior <eecfredes@gmail.com>
2023-11-02 16:42:49 -0300

    :rocket:created fake data to test routes

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
+from typing import Optional
 from fastapi import Body, FastAPI
+from pydantic import BaseModel
+from random import randrange
+
 
 app = FastAPI()
+
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+
+
+data = [
+    {"id": 1, "title": "POST", "content": "Content of the post"},
+    {"id": 2, "title": "POST", "content": "Content of the post"},
+    {"id": 3, "title": "POST", "content": "Content of the post"},
+    {"id": 4, "title": "POST", "content": "Content of the post"},
+]
 
 
 @app.get("/")
@@ -8,12 +27,14 @@ async def root():
     return {"message": "Hello World"}
 
 
-@app.get("/post")
+@app.get("/posts")
 async def get_posts():
-    return {"data": "This is your posts!"}
+    return {"message": data}
 
 
-@app.post("/post")
-async def create_posts(payload: dict = Body(...)):
-    print(payload)
-    return {"post": f"title {payload['title']}"}
+@app.post("/posts")
+async def create_posts(post: Post):
+    post_dict = post.model_dump()
+    post_dict["id"] = randrange(0, 1000000)
+    data.append(post_dict)
+    return {"data": post_dict}
