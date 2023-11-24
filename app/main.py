cddb403e848e13ce01cbffc5453e5449f2ecdc53
Junior <eecfredes@gmail.com>
2023-11-23 22:17:22 -0300

    :rocket: get user route created

--- app/main.py
+++ app/main.py
@@ -139,3 +139,18 @@ async def delete_post(id: int, db: Session = Depends(get_db)):
     # cursor.execute("DELETE FROM posts WHERE id = %s RETURNING *", (str(id),))
     # post = cursor.fetchone()
     # conn.commit()
+
+
+@app.get("/users", status_code=status.HTTP_200_OK)
+def get_users(db: Session = Depends(get_db)):
+    users = db.query(models.User).all()
+    return users
+
+
+@app.post("/users", status_code=status.HTTP_201_CREATED)
+def create_user(db: Session = Depends(get_db)):
+    new_user = models.User(**user.model_dump())
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
