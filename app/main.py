f49d2043cb53c0273ae32c6e5bdb54f00c72fd6f
Junior <eecfredes@gmail.com>
2023-11-23 22:47:22 -0300

    :rocket: create user route created

--- app/main.py
+++ app/main.py
@@ -141,14 +141,14 @@ async def delete_post(id: int, db: Session = Depends(get_db)):
     # conn.commit()
 
 
-@app.get("/users", status_code=status.HTTP_200_OK)
-def get_users(db: Session = Depends(get_db)):
+@app.get("/users", status_code=status.HTTP_200_OK, response_model=List[schemas.UserOut])
+async def get_users(db: Session = Depends(get_db)):
     users = db.query(models.User).all()
     return users
 
 
-@app.post("/users", status_code=status.HTTP_201_CREATED)
-def create_user(db: Session = Depends(get_db)):
+@app.post("/users", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
+async def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     new_user = models.User(**user.model_dump())
     db.add(new_user)
     db.commit()
--- app/schemas.py
+++ app/schemas.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 
 
 class PostBase(BaseModel):
@@ -21,5 +21,14 @@ class Post(PostBase):
 
 
 class UserCreate(BaseModel):
-    email: str
+    email: EmailStr
     password: str
+
+
+class UserOut(BaseModel):
+    id: int
+    email: EmailStr
+    created_at: datetime
+
+    class Config:
+        orm_mode = True
