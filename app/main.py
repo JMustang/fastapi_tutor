c0c443c1ca8e66a870abce6d65cd155c21ebfe17
Junior <eecfredes@gmail.com>
2023-11-26 01:27:46 -0300

    :wastebasket: Cleanning the main file

--- app/main.py
+++ app/main.py
@@ -1,7 +1,5 @@
-from typing import Optional, List
+from typing import List
 from fastapi import FastAPI, Response, status, HTTPException, Depends
-from pydantic import BaseModel
-from random import randrange
 import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
@@ -28,30 +26,9 @@ while True:
         print("Database connection was successful!")
         break
 
-    except Exception as erre:
-        print(f"Erro: {erre}")
+    except Exception as erro:
+        print(f"Erro: {erro}")
         time.sleep(2)
-
-
-def find_post(id):
-    for post in data:
-        if post["id"] == id:
-            return post
-
-
-def find_post_index(id):
-    for i, post in enumerate(data):
-        if post["id"] == id:
-            return i
-    return -1
-
-
-data = [
-    {"id": 1, "title": "POST", "content": "Content of the post"},
-    {"id": 2, "title": "POST", "content": "Content of the post"},
-    {"id": 3, "title": "POST", "content": "Content of the post"},
-    {"id": 4, "title": "POST", "content": "Content of the post"},
-]
 
 
 @app.get("/", status_code=status.HTTP_200_OK)
@@ -63,8 +40,6 @@ async def root():
 async def get_posts(db: Session = Depends(get_db)):
     posts = db.query(models.Post).all()
     return posts
-    # cursor.execute("SELECT * FROM posts")
-    # posts = cursor.fetchall()
 
 
 @app.get("/posts/{id}", status_code=status.HTTP_200_OK, response_model=schemas.Post)
@@ -76,15 +51,6 @@ async def get_post(id: int, db: Session = Depends(get_db)):
             detail=f"Post with id ${id}, was not found!",
         )
     return post
-    # cursor.execute("SELECT * FROM posts WHERE id = %s", (str(id),))
-    # post = cursor.fetchone()
-
-    # if not post:
-    #     raise HTTPException(
-    #         # Essa seria outra forma de retornar erros, usando a lib HTTPException.
-    #         status_code=status.HTTP_404_NOT_FOUND,
-    #         detail=f"Post with id: {id} was not found",
-    #     )
 
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
@@ -94,13 +60,6 @@ async def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(new_post)
     return new_post
-    # cursor.execute(
-    #     "INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING *  ",
-    #     (post.title, post.content, post.published),
-    # )
-
-    # new_post = cursor.fetchone()
-    # conn.commit()
 
 
 @app.put("/posts/{id}", response_model=schemas.Post)
@@ -118,12 +77,6 @@ async def update_post(id: int, post: schemas.PostCreate, db: Session = Depends(g
     db.commit()
 
     return post_query.first()
-    # cursor.execute(
-    #     "UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *",
-    #     (post.title, post.content, post.published, str(id)),
-    # )
-    # updated_post = cursor.fetchone()
-    # conn.commit()
 
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
@@ -137,9 +90,6 @@ async def delete_post(id: int, db: Session = Depends(get_db)):
     post.delete(synchronize_session=False)
     db.commit()
     return Response(status_code=status.HTTP_204_NO_CONTENT)
-    # cursor.execute("DELETE FROM posts WHERE id = %s RETURNING *", (str(id),))
-    # post = cursor.fetchone()
-    # conn.commit()
 
 
 @app.get("/users", status_code=status.HTTP_200_OK, response_model=List[schemas.UserOut])
