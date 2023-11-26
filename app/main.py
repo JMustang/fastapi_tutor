e0cec276183ffaf5fca20f4b6ec7ee5dba0a6c4f
Junior <eecfredes@gmail.com>
2023-11-25 22:51:03 -0300

    :rocket: Get user by id route created

--- app/main.py
+++ app/main.py
@@ -148,6 +148,17 @@ async def get_users(db: Session = Depends(get_db)):
     return users
 
 
+@app.get("/users/{id}", status_code=status.HTTP_200_OK, response_model=schemas.UserOut)
+async def get_user(id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"User with id: {id} does not exist",
+        )
+    return user
+
+
 @app.post("/users", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
 async def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     hashed_password = utils.hash(user.password)
