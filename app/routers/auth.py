734497db659f9e9e1f02d1846f84726b78cd8adf
Junior <eecfredes@gmail.com>
2023-11-30 21:40:11 -0300

    :sparkles: JWT Token

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, status, HTTPException, Response
 from sqlalchemy.orm import Session
 
-from .. import database, schemas, models, utils
+from .. import database, schemas, models, utils, oauth2
 
 
 router = APIRouter(tags=["Authentication"])
@@ -24,5 +24,6 @@ def login(user_credentials: schemas.UserLogin, db: Session = Depends(database.ge
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail=f"Invalid Credentials"
         )
+    access_token = oauth2.create_access_token(data={"user_id": user.id})
 
-    return {"message": "Login Successful"}
+    return {"access_token": access_token, "access_type": "bearer"}
