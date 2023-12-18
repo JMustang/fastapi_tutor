6b6db229258da412bd4904b452fa26ced5d7796d
Junior <eecfredes@gmail.com>
2023-12-17 21:47:05 -0300

    :rocket: Vote schema created2

---
+++ app/routers/vote.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI, Response, status, HTTPException, Depends, APIRouter
+
+router = APIRouter(
+    prefix="/vote",
+    tags=['Vote']
+)
+
+@router.post("/", status_code=status.HTTP_201_CREATED)
+async def vote():
