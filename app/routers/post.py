584b408f883bfc0d1908b1d4d469ae0cfc2b688e
Junior <eecfredes@gmail.com>
2023-12-19 22:50:22 -0300

    :rocket: Doing the same join to the get by id route

--- app/routers/post.py
+++ app/routers/post.py
@@ -48,18 +48,32 @@ async def get_posts(
     return posts
 
 
-@router.get("/{id}", status_code=status.HTTP_200_OK, response_model=schemas.Post)
+@router.get("/{id}", status_code=status.HTTP_200_OK, response_model=schemas.PostOut)
 async def get_post(
     id: int,
     db: Session = Depends(get_db),
     current_user: int = Depends(oauth2.get_current_user),
 ):
-    post = db.query(models.Post).filter(models.Post.id == id).first()
+    # post = db.query(models.Post).filter(models.Post.id == id).first()
+    # if not post:
+    #     raise HTTPException(
+    #         status_code=status.HTTP_404_NOT_FOUND,
+    #         detail=f"Post with id ${id}, was not found!",
+    #     )
+
+    post = (
+        db.query(models.Post, func.count(models.Vote.post_id).label("votes"))
+        .join(models.Vote, models.Vote.post_id == models.Post.id, isouter=True)
+        .group_by(models.Post.id)
+        .filter(models.Post.id == id)
+        .first()
+    )
     if not post:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f"Post with id ${id}, was not found!",
         )
+
     return post
 
 
